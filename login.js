ef4068305940ae9ac75c767b21f92978b5847523
makerealmagic <rackyrockstar2002@gmail.com>
2023-08-12 10:03:26 +0530

    login

--- login.js
+++ login.js
@@ -1,1 +1,13 @@
-
+function main(){
+    var uname = login.uname.value;
+    var pwd = login.pwd.value;
+    if(uname == ""){
+        alert("please enter the username");
+        return false;
+    }
+    if(pwd == ""){
+        alert("please enter the password");
+        return false;
+    }
+    return true;
+}
